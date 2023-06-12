76998db14bfa925f9efa10847070cb32313ecd82
osvaFernandez <osvanaranjo96@gmail.com>
2023-06-12 11:10:48 -0400

    login, logout forms

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
     return render(request, "main/home.html")
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/sign-up.html', {'form': form})
